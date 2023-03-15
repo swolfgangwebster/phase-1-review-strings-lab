8f2a3814da3c89008c6ceea85132b78f4a7228cb
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-14 20:55:38 -0500

    stwings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "cat"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase() ;
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
